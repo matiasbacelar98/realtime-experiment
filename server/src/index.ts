3ecf2346c83c994a34cbd8c28d1c25637c2f1bf6
Matias Bacelar <matiasbacelar@gmail.com>
2024-10-06 13:52:48 -0300

    server: Express server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Working...');
+import express from 'express';
+// import socketIO from 'socket.io';
+
+/** Constants */
+const PORT = process.env.PORT || '5000';
+
+/** Server setup */
+const app = express();
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.send({ response: 'I am alive' }).status(200);
+});
+
+/** Run server */
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
